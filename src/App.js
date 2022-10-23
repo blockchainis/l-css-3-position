aa4fa7532b13ce3dc8bf48bae742c095affd591e
hoon2 <sanghoon.engineer@gmail.com>
2022-10-24 06:55:32 +0900

    position 초기코드

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import GlobalStyle from "./GlobalStyle";
+import styled from "styled-components";
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BlueBox = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: blue;
+  border: 1px solid black;
+`;
+const RedBox = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: red;
+  border: 1px solid black;
+`;
+
+const GreenBox = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: green;
+  border: 1px solid black;
+`;
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <div>안녕</div>
+      <Container>
+        <BlueBox></BlueBox>
+        <RedBox></RedBox>
+        <GreenBox></GreenBox>
+      </Container>
     </>
   );
 }
